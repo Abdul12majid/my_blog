b7e5f932a9b754e0785e3a6655433b36d55aae03
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-03 21:03:09 +0100

    render post index

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-	return render(request, 'index.html')
+	the_blog = Post.objects.order_by('-likes').first()
+	context = {
+	'the_blog':the_blog,
+	}
+	return render(request, 'index.html', context)
