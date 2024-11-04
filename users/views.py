6db8d4313d0b73a3c0b8e38dcd3ca101c8753b09
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-04 21:08:32 +0100

    sign-in view

--- users/views.py
+++ users/views.py
@@ -3,16 +3,31 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-	return render(request, "signin.html")
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password1']
+		user = User.objects.filter(username=username).exists()
+		if user is True:
+			get_user = User.objects.get(username=username)
+			user_username = get_user.username
+			auth_user = authenticate(request, username=user_username, password=password)
+			if auth_user is not None:
+				login(request, get_user)
+				return redirect('index')
+			return render(request, 'login.html')
+		return redirect('register')
+	return render(request, 'login.html')
 
 
 def signup(request):
 	if request.POST:
+
 		username = request.POST['username']
 		email = request.POST['email']
 		password1 = request.POST.get('password1')
 		password2 = request.POST.get('password2')
-		if password1 == password2:
+
+		if password1 != password2:
 			print('Password mismatch')	
 		if User.objects.filter(username=username).exists():
 			print('username exists')	
@@ -22,4 +37,5 @@ def signup(request):
 		get_user = authenticate(username=username, password=password2)
 		login(request, get_user)
 		return redirect('index')
+
 	return render(request, 'register.html')
