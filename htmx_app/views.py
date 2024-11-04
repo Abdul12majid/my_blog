064aae83f6e1818322d9ccf59a8de469cc868933
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-04 19:27:24 +0100

    checkusername dynamic sign-in view

--- htmx_app/views.py
+++ htmx_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
 	return HttpResponse("htmx")
+
+
+def check_username(request):
+	username = request.POST.get('username')
+	if User.objects.filter(username=username).exists():
+		return HttpResponse("Username found")
+	else:
+		return HttpResponse("Username not found")
