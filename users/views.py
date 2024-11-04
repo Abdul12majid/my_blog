11ab0e1ebf2ff1f54b79612aa5d05b30747f5687
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-04 21:04:13 +0100

    sign-up view

--- users/views.py
+++ users/views.py
@@ -7,4 +7,21 @@ def index(request):
 
 
 def signup(request):
-	return render(request, 'signup.html')
+	if request.POST:
+		username = request.POST['username']
+		email = request.POST['email']
+		password1 = request.POST.get('password1')
+		password2 = request.POST.get('password2')
+		if password1 == password2:
+			if User.objects.filter(username=username).exists():
+				return HttpResponse("<p class='success'><b></b></p>")
+			else:
+				user = User.objects.create_user(username=username, password=password2)
+				user.save()
+				get_user = authenticate(username=username, password=password2)
+				login(request, get_user)
+				return redirect('index')
+		else:
+			print('Password mismatch')
+			return redirect('register')
+	return render(request, 'register.html')
